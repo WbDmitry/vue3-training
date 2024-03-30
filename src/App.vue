c154bb8202be3a14852e8622bb70dfa560478c4c
Wb Dmitry <wb.dmitry@yandex.ru>
2024-03-30 19:28:50 +0300

    Вывел свойства из объекта data в приложение

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
 export default {
-	name: 'App'
+	name: 'App',
+	data() {
+		return {
+			firstName: 'Dmitry',
+			lastName: 'Wb',
+		}
+	}
 }
 </script>
 
 <template>
 	<p>Что такое Lorem Ipsum?</p>
+	<div>
+		{{ lastName }}
+	</div>
+	<div>
+		{{ firstName }}
+	</div>
 </template>
 
 <style>
